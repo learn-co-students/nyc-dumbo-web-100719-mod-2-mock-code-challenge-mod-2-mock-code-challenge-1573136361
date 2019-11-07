ce05fbdf3febcb0043a36bf0cc9c36bf4193648d
Ian Hollander <ianholla@gmail.com>
2019-11-07 12:58:49 -0500

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_175322) do
+ActiveRecord::Schema.define(version: 2019_01_02_175158) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -24,16 +24,6 @@ ActiveRecord::Schema.define(version: 2019_01_02_175322) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "signups", force: :cascade do |t|
-    t.integer "activity_id"
-    t.integer "camper_id"
-    t.integer "time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["activity_id"], name: "index_signups_on_activity_id"
-    t.index ["camper_id"], name: "index_signups_on_camper_id"
   end
 
 end
